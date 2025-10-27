7997dda27deac9677668b41e0e219937d35aad7f
이정현 <hyune7142@daum.net>
2025-10-27 17:06:05 +0900

    핵심역량 카드 사이즈 조정

--- src/components/expertise/ExpertiseItem.tsx
+++ src/components/expertise/ExpertiseItem.tsx
@@ -15,12 +15,9 @@ interface ExpertiseItemProps {
 export default function ExpertiseItem({ item }: ExpertiseItemProps) {
   return (
     <div className="flex flex-col overflow-visible">
-      {' '}
-      {/* ✅ 부모 overflow-visible */}
-      <Card className="h-[420px] overflow-hidden transition-transform duration-300">
+      <Card className="h-[350px] overflow-hidden transition-transform duration-300">
         <CardContent className="flex h-full flex-col p-0 text-center">
-          {/* 이미지 영역 */}
-          <div className="relative h-[230px] w-full">
+          <div className="relative h-[230px] w-full overflow-hidden">
             <Image
               src={item.img}
               alt={item.title}
@@ -29,9 +26,7 @@ export default function ExpertiseItem({ item }: ExpertiseItemProps) {
               sizes="(max-width: 768px) 100vw, 33vw"
             />
           </div>
-
-          {/* 텍스트 영역 */}
-          <div className="flex h-[190px] flex-col items-center p-4 text-center">
+          <div className="flex h-[200px] flex-col items-center p-4 text-center">
             <H4 className="mb-[10px]">{item.title}</H4>
             <Lead className="leading-relaxed break-keep">{item.desc}</Lead>
           </div>
